f94c24830488fbb75188f5e2ac1bb5924dbf9b27
JaimeTerrero <jaimedavidterre@gmail.com>
2024-02-28 23:16:41 -0400

    file moved to specific folder

--- VictorVentral.Products.Domain/obj/Debug/net8.0/VictorVentral.Products.Domain.AssemblyInfo.cs
+++ VictorVentral.Products.Domain/obj/Debug/net8.0/VictorVentral.Products.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VictorVentral.Products.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada8dee99d38b870476a2e8b2bd95df2682d750f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VictorVentral.Products.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VictorVentral.Products.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
